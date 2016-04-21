c1f516e58619874a1078b19650faccad8c9dad62
Matt Dyke <matthew.dyke@mines.sdsmt.edu>
2016-04-21 17:27:30 -0600

    Added most of remaining editable fields.

--- edit-student.php
+++ edit-student.php
@@ -2,43 +2,71 @@
 //Read in student data from XML file
 $studentXML = new SimpleXMLElement($_POST['name'], $options = 0, $data_is_url = true);
 
+//Get contact info
+$schoolEmail = $studentXML->contactInfo->schoolEmail;
+$personalEmail = $studentXML->contactInfo->personalEmail;
+$phoneNum = $studentXML->contactInfo->phoneNum;
+
 //Build link back to page being edited
 $returnLink = "http://localhost/SDSMT_Web/student.php?name=" . substr( $_POST['name'], 9 );
 
 echo <<<_END
-        <html lang="en">
-            <head>
-                <title>$studentXML->name</title>
-                <link rel="stylesheet" type="text/css" href="student.css" />
-                <meta charset="utf-8"/>
-            </head>
-            
-            <body>
-                <!-- Fixed Header -->
-                <div id="fixedheader">
-                    Header stuff
-                </div>
+<html lang="en">
+    <head>
+        <title>$studentXML->name</title>
+        <link rel="stylesheet" type="text/css" href="student.css" />
+        <meta charset="utf-8"/>
+    </head>
+    
+    <body>
+        <!-- Fixed Header -->
+        <div id="fixedheader">
+            Header stuff
+        </div>
 
-                <!-- Student content pane -->
-                <div id="studentContent">
-                    <form action="save-file.php" method="post">
-                        Name: <input type="text" name="name" value="$studentXML->name"><br/>
-                        Picture Link: <input type="text" name="picture" value="$studentXML->picture"><br/>
-                        <br/>
-                                               
-                        
-                        <!-- Save, Reset, & Cancel buttons -->
-                        <br/><input type="submit" value="Save"/>
-                        <input type="reset"/>
-                        <a href=$returnLink><input type="button" value="Cancel"></a>
-                    </form>
-                </div>
+        <!-- Student content pane -->
+        <div id="studentContent">
+            <form action="save-file.php" method="post">
+                <!-- Student name & picture fields -->
+                Name: <input type="text" name="name" value="$studentXML->name"><br/>
+                Picture Link: <input type="text" name="picture" value="$studentXML->picture"><br/>
+                <br/>
                 
-                <!-- Fixed Footer -->
-                <div id="fixedfooter">
-                    Footer stuff
-                </div>
-            </body>
-        </html>
+                <!-- Degrees & Graduation Date -->
+                
+                Graduation Date: <input type="text" name="gradDate" value="$studentXML->gradDate"><br/>
+                <br/>
+                
+                <!-- Contact Information -->
+                School Email: <input type="text" size="30" name=schoolEmail" value="$schoolEmail"><br/>
+                Personal Email: <input type="text" size="30" name=personalEmail" value="$personalEmail"><br/>
+                Phone Number: <input type="text" name=phoneNum" value="$phoneNum"><br/>
+                <br/>
+                
+                <!-- Courses & Interest Area -->
+                
+                Area of Interest: <input type="text" name=interestArea value="$studentXML->interestArea"><br/>
+                <br/>
+                
+                <!-- External Links -->
+                Student Webpage: <input type="text" size="50" name=studentWebsite value="$studentXML->studentWebsite"><br/>
+                LinkedIn: <input type="text" size="50" name=LinkedIn value="$studentXML->LinkedIn"><br/>
+                GitHub: <input type="text" size="50" name=GitHub value="$studentXML->GitHub"><br/>
+                KnewRecruit: <input type="text" size="50" name=KnewRecruit value="$studentXML->KnewRecruit"><br/>
+                
+                <!-- Save, Reset, & Cancel buttons -->
+                <br/><input type="submit" value="Save"/>
+                <input type="reset"/>
+                <a href=$returnLink><input type="button" value="Cancel"></a>
+            </form>
+            
+        </div>
+        
+        <!-- Fixed Footer -->
+        <div id="fixedfooter">
+            Footer stuff
+        </div>
+    </body>
+</html>
 _END;
 ?>
