c81c51c6998f96bd1ae7d50a609a593db38afc58
Matt Dyke <matthew.dyke@mines.sdsmt.edu>
2016-04-21 17:48:14 -0600

    Started writing PHP to save edited XML file.

--- save-file.php
+++ save-file.php
@@ -1,3 +1,19 @@
 <?php
-    echo $_POST['name'];
+    //Adapted from http://stackoverflow.com/questions/3552755/how-to-save-xml-using-php
+    //Taken on 4/21/2016
+    //Written by StackOverFlow user vfn
+    
+    //Creates XML string and XML document using the DOM
+    $dom = new DomDocument( "1.0", "UTF-8" );
+    
+    //Add student root
+    $student = $dom->appendChild( $dom->createElement( "student" ) );
+    
+    //Add name element to student root
+    $nodeName = $dom->createElement( "name", "MATT" );
+    $student->appendChild( $nodeName );
+    
+    //Save XML document
+    $dom->formatOutput = true;
+    $dom->save( "test.xml" );    
 ?>
