b82d7fa321f9166c42063230194ca9372d5f2c3c
Matt Dyke <matthew.dyke@mines.sdsmt.edu>
2016-04-21 19:47:03 -0600

    Added more elements to XML file saver.

--- save-file.php
+++ save-file.php
@@ -1,8 +1,4 @@
 <?php
-    //Adapted from http://stackoverflow.com/questions/3552755/how-to-save-xml-using-php
-    //Taken on 4/21/2016
-    //Written by StackOverFlow user vfn
-    
     //Creates XML string and XML document using the DOM
     $dom = new DomDocument( "1.0", "UTF-8" );
     
@@ -10,12 +6,61 @@
     $student = $dom->appendChild( $dom->createElement( "student" ) );
     
     //Add name element to student root
-    $nodeName = $dom->createElement( "name", "MATT" );
+    $nodeName = $dom->createElement( "name", $_POST['name'] );
     $student->appendChild( $nodeName );
+    
+    //Add picture element to student root
+    $nodePicture = $dom->createElement( "picture", $_POST['picture'] );
+    $student->appendChild( $nodePicture );
+    
+    //Add degrees element to student root
+    $nodeDegrees = $dom->createElement( "degrees", "" );
+    $student->appendChild( $nodeDegrees );
+    
+    //Add gradDate element to student root
+    $nodeGradDate = $dom->createElement( "gradDate", $_POST['gradDate'] );
+    $student->appendChild( $nodeGradDate );
+    
+    //Add contactInfo element to student root
+    $nodeContactInfo = $dom->createElement( "contactInfo" );
+    $student->appendChild( $nodeContactInfo );
+    
+    //Add schoolEmail element to contactInfo element
+    $nodeSchoolEmail = $dom->createElement( "schoolEmail", $_POST['schoolEmail'] );
+    $nodeContactInfo->appendChild( $nodeSchoolEmail );
+    
+    //Add personalEmail element to contactInfo element
+    $nodePersonalEmail = $dom->createElement( "personalEmail", $_POST['personalEmail'] );
+    $nodeContactInfo->appendChild( $nodePersonalEmail );
+    
+    //Add phoneNum element to contactInfo element
+    $nodePhoneNum = $dom->createElement( "phoneNum", $_POST['phoneNum'] );
+    $nodeContactInfo->appendChild( $nodePhoneNum );
+    
+    //Add interestArea element to student root
+    $nodeInterestArea = $dom->createElement( "interestArea", $_POST['interestArea'] );
+    $student->appendChild( $nodeInterestArea );
+    
+    //Add studentWebsite element to student root
+    $nodeStudentWebsite = $dom->createElement( "studentWebsite", $_POST['studentWebsite'] );
+    $student->appendChild( $nodeStudentWebsite );
+    
+    //Add LinkedIn element to student root
+    $nodeLinkedIn = $dom->createElement( "LinkedIn", $_POST['LinkedIn'] );
+    $student->appendChild( $nodeLinkedIn );
+    
+    //Add GitHub element to student root
+    $nodeGitHub = $dom->createElement( "GitHub", $_POST['GitHub'] );
+    $student->appendChild( $nodeGitHub );
+    
+    //Add KnewRecruit element to student root
+    $nodeKnewRecruit = $dom->createElement( "KnewRecruit", $_POST['KnewRecruit'] );
+    $student->appendChild( $nodeKnewRecruit );
     
     //Save XML document
     $dom->formatOutput = true;
     $dom->save( "test.xml" );
     
+    //After file is saved, redirect user to the related student page
     header( "Location: http://localhost/SDSMT_Web/student.php?name=matthew-dyke.xml" );
 ?>
