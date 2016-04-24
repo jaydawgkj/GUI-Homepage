fd0fd22c614cf5b9c75d66186a9e1328aa7b7123
Matt Dyke <matthew.dyke@mines.sdsmt.edu>
2016-04-24 13:38:45 -0600

    Finished initDegrees JavaScript.

--- student-scripts.js
+++ student-scripts.js
@@ -1,7 +1,21 @@
 var numDegrees = 0;
 
-function initDegrees( degrees ) {
-    addDegree(degrees.value);
+function initDegrees( degrees ) { 
+    //Get index of first ; in degree string
+    var index = degrees.value.indexOf( ";" );
+    
+    //Loop over all semicolons in degrees string
+    while( index > 0 )
+    {
+        //Add a field for current degree
+        addDegree( degrees.value.substring(0, index ) );
+        
+        //Trim degrees string to remove current degree
+        degrees.value = degrees.value.substring( index+1, degrees.value.length )
+        
+        //Find index of next ; in remaining degree string
+        index = degrees.value.indexOf( ";" );
+    }
 };
 
 function addDegree( degreeName ) {
@@ -11,6 +25,7 @@ function addDegree( degreeName ) {
     input.name = 'degree_';
     input.name = input.name.concat( numDegrees++ );
     input.value = degreeName;
+    input.size = 40;
     div.appendChild(document.createElement('br'));
     div.appendChild(input);
 };
